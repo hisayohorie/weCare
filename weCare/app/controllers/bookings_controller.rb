290d4c90c90f1c7a4dfd05238f7e37aaff913eb3
hisayohorie <hisayohorie@gmail.com>
2016-05-23 19:51:15 -0400

    booking controller, profile controller, review controller done

--- weCare/app/controllers/bookings_controller.rb
+++ weCare/app/controllers/bookings_controller.rb
@@ -1,22 +1,49 @@
 class BookingsController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
+    @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    if current_user
+      @booking = Booking.new
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, alert: "Please login."
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if current_user
+      if @booking.update_attributes(booking_params)
+        redirect_to @booking
+      else
+        renser :edit
+      end
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
   end
+end
+
+private
+
+def booking_params
+  params.require(:booking).permit(:user_id, :date_time, :address, :num_of_hours, :service_id)
 end
