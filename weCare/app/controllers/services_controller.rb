150b3675cd73b5e85b467c88ec19f0e6f1d4e6fd
Erin Kim <kim.erin.kim@gmail.com>
2016-05-23 19:32:45 -0400

    created controller for service

--- weCare/app/controllers/services_controller.rb
+++ weCare/app/controllers/services_controller.rb
@@ -1,22 +1,66 @@
 class ServicesController < ApplicationController
+  before_action :login, only: %i(edit update)
+
   def index
+    @services = Service.all
   end
+end
 
   def new
+    @service = Service.new
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   def create
+
+    if current_user
+      @service = current_user.build(service_params)
+      if @service.save
+        redirect_to @service
+      else
+        render :new
+      end
+    else
+      render :new, alert: "You need to login"
+    end
+
   end
 
   def edit
+    @service = Service.find(params[:id])
+    if current_user
+      if @service.update_attributes(service_params)
+        redirect_to @service
+      else
+        render :edit
+      end
+    end
   end
 
   def show
+    @service = Service.find(params[:id])
   end
 
   def update
+    @service = Service.find(params[:id])
+    if current_user
+      if @service.update_attributes(service_params)
+        redirect_to @service
+      else
+        render :edit
+      end
+    end
   end
 
   def destroy
   end
+
+  private
+  def service_params
+    params.require(:project).permit(:profile_id, :infant_care, :toddler_care, :tween_care, :teenage_care)
 end
--- weCare/app/controllers/users_controller.rb
+++ weCare/app/controllers/users_controller.rb
@@ -15,7 +15,27 @@ class UsersController < ApplicationController
    end
  end
 
+ def show
+   @user = User.find(params[:id])
+ end
+
+def edit
+  @user = User.find(current_user)
+end
+
+def update
+  @user = User.find(current_user)
+  if @user.update_attributes(user_params_edit)
+    redirect_to current_user
+  else
+    render :edit
+  end
+end
+
  private
+ def user_params_edit
+   params.require(:user).permit(:name, :email)
+ end
 
  def user_params
    params.require(:user).permit(:email, :password, :password_confirmation, :name)
