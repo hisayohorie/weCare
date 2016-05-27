a2549d60984448ccb6c95b338687360001f07b33
zap-rowsdower <brock.whitbread@gmail.com>
2016-05-27 18:58:54 -0400

    include js file for homepage, add function for bg image changes

---
+++ weCare/app/assets/javascripts/home.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  /*! slides | https://gist.github.com/mhulse/66bcbb7099bb4beae530 */
+  (function($) {
+
+  	'use strict';
+
+  	var $slides = $('[data-slides]');
+  	var images = $slides.data('slides');
+  	var count = images.length;
+  	var slideshow = function() {
+  		$slides
+  			.css('background-image', 'url("' + images[Math.floor(Math.random() * count)] + '")')
+  			.show(0, function() {
+  				setTimeout(slideshow, 2000);
+  			});
+  	};
+
+  	slideshow();
+
+  }(jQuery));
+});
