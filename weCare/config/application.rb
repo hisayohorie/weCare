0e28d2d8294c2de2c15d6cdf16688d1ec9c512c5
zap-rowsdower <brock.whitbread@gmail.com>
2016-05-27 18:59:58 -0400

    add config line to make fonts work

--- weCare/config/application.rb
+++ weCare/config/application.rb
@@ -23,5 +23,6 @@ module WeCare
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.settings = File.join(Rails.root, 'config', 'application.yml')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
