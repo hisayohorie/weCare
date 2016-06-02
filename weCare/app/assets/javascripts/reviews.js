aa1c5b53dd8bff33aea09c26318ec20623831727
hisayohorie <hisayohorie@gmail.com>
2016-06-01 21:39:28 -0400

    working on star css

--- weCare/app/assets/javascripts/reviews.js
+++ weCare/app/assets/javascripts/reviews.js
@@ -1,3 +1,18 @@
 $(document).on('ready', function(){
+  $('#rating-1').on('click', function (){
+    $('#one-star').css("color","#40E0D0")
+  });
+  $('#rating-2').on('click', function(){
+    $('#one-star, #two-star').css("color","#40E0D0")
+  });
+  $('#rating-3').on('click', function(){
+    $('#one-star, #two-star, #three-star').css("color","#40E0D0" )
+  });
+  $('#rating-4').on('click', function(){
+    $('#one-star, #two-star, #three-star, #four-star').css("color","#40E0D0" )
+  });
 
+  $('#rating-5').on('click', function(){
+    $('.fa').css("color","#40E0D0" )
+  });
 });
