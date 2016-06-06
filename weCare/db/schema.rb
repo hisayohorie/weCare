092d94df7186ab6398ea9a3b315cdb1235cca1e3
Erin Kim <kim.erin.kim@gmail.com>
2016-06-06 17:07:19 -0400

    test

--- weCare/db/schema.rb
+++ weCare/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160606013225) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookings", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "address"
@@ -82,7 +85,7 @@ ActiveRecord::Schema.define(version: 20160606013225) do
     t.datetime "remember_me_token_expires_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
 end
