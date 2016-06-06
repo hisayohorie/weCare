c8850e4cde954863a9e7108a05f947369c3f94ef
hisayohorie <hisayohorie@gmail.com>
2016-06-06 13:38:13 -0400

    review show as stars

--- weCare/app/models/review.rb
+++ weCare/app/models/review.rb
@@ -8,7 +8,9 @@ class Review < ActiveRecord::Base
     reviewer = self.user
     relevant_bookings = reviewer.bookings.where(profile_id: self.profile_id)
     if relevant_bookings.length <= 0
-      errors.add(:profile_id, "You never worked with this person, we know.")
+      errors.add(:profile_id, "You never worked with this robot, to write a review, book an appointment!.")
     end
   end
+
+
 end
